95dcc9f91d127dbebb711170e5bf5e2b390b8fd1
AbdullahSK03 <abdullah.sajid1974@gmail.com>
2024-04-06 14:29:44 +0530

    added CRUD operation

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,10 +1,12 @@
 const asyncHandler = require('express-async-handler');
 
+const Goal = require('../models/goalModel');
 //@desc     GET goals
 //@route    GET /api/goals
 //@access   Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "get goals" });
+    const goals = await Goal.find();
+    res.status(200).json(goals);
 })
 
 //@desc     GET goals
@@ -16,21 +18,59 @@ const setGoals = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field');
     };
 
-    res.status(200).json({ message: "set goals" });
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(goal);
 })
 
 //@desc     GET goals
 //@route    PUT /api/goals
 //@access   Private
 const updateGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `update goal ${req.params.id}` });
-})
+    const goal = await Goal.findById(req.params.id);
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found');
+    } else {
+        // Declare updatedGoal outside of the else block
+        let updatedGoal;
+
+        updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        });
+
+        // Now you can access updatedGoal outside of the else block
+        res.status(200).json(updatedGoal);
+    }
+});
 
 //@desc     GET goals
 //@route    DELETE /api/goals
 //@access   Private
 const deleteGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `delete goal ${req.params.id}`});
+
+    // const goal = await Goal.findById(req.params.id);
+    // if(!goal){
+    //     res.status(400)
+    //     throw new Error('Goal not found');
+    // }
+
+    // await goal.remove();
+
+
+    // res.status(200).json({id: req.params.id});
+    const goal = await Goal.findById(req.params.id);
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found');
+    }
+
+    await goal.deleteOne({ _id: req.params.id });
+    res.status(200).json({ id: req.params.id });
 })
 
 module.exports = {
