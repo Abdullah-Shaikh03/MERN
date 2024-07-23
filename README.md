5766e6898dbd3ae558dfcdc04993b38bd5d258d8
AbdullahSK03 <abdullah.sajid1974@gmail.com>
2024-07-23 23:34:37 +0530

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# Course Link
-https://www.youtube.com/playlist?list=PLillGF-RfqbbQeVSccR9PGKHzPJSWqcsm
--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {register, userLogin, getMe} = require('../controllers/userControllers');
-const {protect} = require('../Middleware/authMiddleware');
-router.post('/', register);
-router.post('/login', userLogin);
-router.get('/me', protect, getMe);
-
-module.exports = router
